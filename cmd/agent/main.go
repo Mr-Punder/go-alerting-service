b9c008081833e09a20447748d2d8ceb0e21461d0
Mr-Punder <goomer125@gmail.ru>
2023-10-02 15:38:43 +0300

    ite3 is completed

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -23,11 +23,11 @@ func main() {
 }
 
 func sendMetrics(metrics []metrics.Metric, serverAddress string) error {
-	initUrl := fmt.Sprintf("%s/update", serverAddress)
+	init := fmt.Sprintf("%s/update", serverAddress)
 	client := resty.New()
 
 	for _, metric := range metrics {
-		url := fmt.Sprintf("%s/%s/%s/%s", initUrl, metric.Type, metric.Name, metric.Val)
+		url := fmt.Sprintf("%s/%s/%s/%s", init, metric.Type, metric.Name, metric.Val)
 
 		resp, err := client.R().SetHeader("Content-Type", "text/plain").Post(url)
 		if err != nil {
