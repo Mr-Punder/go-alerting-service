aacab76684225010d9d3265fb38ac0f2c276a37b
Mr-Punder <goomer125@gmail.ru>
2023-10-04 14:17:50 +0300

    iter5 is complited

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -17,6 +20,32 @@ func parseFlags() {
 	flag.IntVar(&rawPollInterval, "r", 2, "poll interval")
 	flag.IntVar(&rawReportInterval, "p", 10, "report interval")
 	flag.Parse()
+
+	if envAddrs, ok := os.LookupEnv("ADDRESS"); ok {
+		serverAddress = envAddrs
+	}
+
+	if envPollInterval, ok := os.LookupEnv("POLL_INTERVAL"); ok {
+		var rawPollInterval64 int64
+		var err error
+		if rawPollInterval64, err = strconv.ParseInt(envPollInterval, 10, 64); err != nil {
+			log.Fatal("wrong report interval")
+		}
+		rawPollInterval = int(rawPollInterval64)
+
+	}
+
+	if envReportInterval, ok := os.LookupEnv("REPORT_INTERVAL"); ok {
+		var rawReportInterval64 int64
+		var err error
+		if rawReportInterval64, err = strconv.ParseInt(envReportInterval, 10, 64); err != nil {
+			log.Fatal("wrong report interval")
+		}
+		rawReportInterval = int(rawReportInterval64)
+
+	}
+
 	pollInterval = time.Duration(rawPollInterval) * time.Second
 	reportInterval = time.Duration(rawReportInterval) * time.Second
+
 }
