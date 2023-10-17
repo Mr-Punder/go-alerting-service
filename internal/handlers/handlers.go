bc979a636c0377c0a11d90cbd51a3b0a4bac4d57
Mr-Punder <goomer125@gmail.ru>
2023-10-17 15:49:26 +0300

    final 1 sprint

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -10,128 +10,131 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+type Handler struct {
+	stor storage.MemStor
+}
+
+func NewHandler(stor storage.MemStor) *Handler {
+	return &Handler{stor}
+}
+
 func MetricRouter(storage storage.MemStor) chi.Router {
 	r := chi.NewRouter()
 
+	handler := NewHandler(storage)
+
 	return r.Route("/", func(r chi.Router) {
-		r.Get("/", ShowAllHandler(storage))
+		r.Get("/", handler.ShowAllHandler)
 		r.Route("/update", func(r chi.Router) {
-			r.Post("/{type}/{name}/{value}", UpdHandler(storage))
+			r.Post("/{type}/{name}/{value}", handler.UpdHandler)
 		})
-		r.Get("/value/{type}/{name}", ValueHandler(storage))
-		r.Get("/{}", DefoultHandler)
-		r.Post("/{}", DefoultHandler)
+		r.Get("/value/{type}/{name}", handler.ValueHandler)
+		r.Get("/{}", handler.DefoultHandler)
+		r.Post("/{}", handler.DefoultHandler)
 	})
 }
 
-func UpdHandler(stor storage.MemStor) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) UpdHandler(w http.ResponseWriter, r *http.Request) {
 
-		if r.Method != http.MethodPost {
-			http.Error(w, "Only POST requests are allowed for update!", http.StatusMethodNotAllowed)
-			return
-		}
-
-		tp := chi.URLParam(r, "type")
-		name := chi.URLParam(r, "name")
-		val := chi.URLParam(r, "value")
-
-		switch tp {
-		case "gauge":
-			fval, err := strconv.ParseFloat(val, 64)
-			if err != nil {
-				http.Error(w, "wrong format value", http.StatusBadRequest)
-				return
-			}
-
-			if err := stor.SetGouge(name, fval); err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-		case "counter":
-			ival, err := strconv.ParseInt(val, 10, 64)
-			if err != nil {
-				http.Error(w, "wrong format value", http.StatusBadRequest)
-				return
-			}
-
-			if err := stor.SetCounter(name, ival); err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-		default:
-			http.Error(w, "wrong type", http.StatusBadRequest)
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
-
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST requests are allowed for update!", http.StatusMethodNotAllowed)
+		return
 	}
+
+	tp := chi.URLParam(r, "type")
+	name := chi.URLParam(r, "name")
+	val := chi.URLParam(r, "value")
+
+	switch tp {
+	case "gauge":
+		fval, err := strconv.ParseFloat(val, 64)
+		if err != nil {
+			http.Error(w, "wrong format value", http.StatusBadRequest)
+			return
+		}
+
+		if err := h.stor.SetGouge(name, fval); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	case "counter":
+		ival, err := strconv.ParseInt(val, 10, 64)
+		if err != nil {
+			http.Error(w, "wrong format value", http.StatusBadRequest)
+			return
+		}
+
+		if err := h.stor.SetCounter(name, ival); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	default:
+		http.Error(w, "wrong type", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 
 }
 
-func DefoultHandler(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) DefoultHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "wrong requests", http.StatusBadRequest)
 }
 
-func ValueHandler(stor storage.MemStor) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			http.Error(w, "Only Get requests are allowed for value!", http.StatusMethodNotAllowed)
+func (h *Handler) ValueHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Only Get requests are allowed for value!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	tp := chi.URLParam(r, "type")
+	name := chi.URLParam(r, "name")
+	w.Header().Set("Content-Type", "text/plain")
+	switch tp {
+	case "gauge":
+		val, ok := h.stor.GetGouge(name)
+		if !ok {
+			http.Error(w, fmt.Sprintf("%s not found", name), http.StatusNotFound)
 			return
 		}
-
-		tp := chi.URLParam(r, "type")
-		name := chi.URLParam(r, "name")
-		w.Header().Set("Content-Type", "text/plain")
-		switch tp {
-		case "gauge":
-			val, ok := stor.GetGouge(name)
-			if !ok {
-				http.Error(w, fmt.Sprintf("%s not found", name), http.StatusNotFound)
-				return
-			}
-			w.Write([]byte(strconv.FormatFloat(val, 'f', -1, 64)))
-		case "counter":
-			val, ok := stor.GetCounter(name)
-			if !ok {
-				http.Error(w, fmt.Sprintf("%s not found", name), http.StatusNotFound)
-				return
-			}
-			w.Write([]byte(strconv.FormatInt(val, 10)))
-		default:
-			http.Error(w, "wrong type", http.StatusBadRequest)
+		w.Write([]byte(strconv.FormatFloat(val, 'f', -1, 64)))
+	case "counter":
+		val, ok := h.stor.GetCounter(name)
+		if !ok {
+			http.Error(w, fmt.Sprintf("%s not found", name), http.StatusNotFound)
+			return
 		}
-
+		w.Write([]byte(strconv.FormatInt(val, 10)))
+	default:
+		http.Error(w, "wrong type", http.StatusBadRequest)
 	}
+
 }
 
-func ShowAllHandler(stor storage.MemStor) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			http.Error(w, "Only Get requests are allowed for value!", http.StatusMethodNotAllowed)
-			return
-		}
-
-		html := "<html><body>"
-
-		html += "<h2>Gauge:</h2>"
-		metrics := []string{}
-		for key, val := range stor.GetAllGauge() {
-			metrics = append(metrics, fmt.Sprintf("<p>%s: %f</p>", key, val))
-		}
-		sort.Strings(metrics)
-		for _, str := range metrics {
-			html += str
-		}
-		html += "<h2>Counter:</h2>"
-		for key, val := range stor.GetAllCounter() {
-			html += fmt.Sprintf("<p>%s: %d</p>", key, val)
-		}
-		html += "</body></html>"
-		w.Header().Set("Content-Type", "text/html")
-
-		w.Write([]byte(html))
-
+func (h *Handler) ShowAllHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Only Get requests are allowed for value!", http.StatusMethodNotAllowed)
+		return
 	}
+
+	html := "<html><body>"
+
+	html += "<h2>Gauge:</h2>"
+	metrics := []string{}
+	for key, val := range h.stor.GetAllGauge() {
+		metrics = append(metrics, fmt.Sprintf("<p>%s: %f</p>", key, val))
+	}
+	sort.Strings(metrics)
+	for _, str := range metrics {
+		html += str
+	}
+	html += "<h2>Counter:</h2>"
+	for key, val := range h.stor.GetAllCounter() {
+		html += fmt.Sprintf("<p>%s: %d</p>", key, val)
+	}
+	html += "</body></html>"
+	w.Header().Set("Content-Type", "text/html")
+
+	w.Write([]byte(html))
+
 }
